3c71fd1dc95cdf31274d7428d171f88d118d7b13
BruceLDs <2435324639@qq.com>
2019-10-23 20:49:00 +0800

    project

---
+++ C++入门/test.cpp
@@ -0,0 +1,46 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+using namespace std;
+//void test(){
+//	int a = 10;
+//	int& ra = a;
+//	cout << a << endl;
+//	cout << ra << endl;
+//	ra = 195;
+//	cout << a << endl;
+//	cout << ra << endl;
+//}
+//void swap(int& a,int& b){
+//	int tmp = a;
+//	a = b;
+//	b = tmp;
+//}
+//int main(){
+//	test();
+//	int a = 2;
+//	int b = 3;
+//	swap( a, b);
+//	cout << a << endl;
+//	cout << b << endl;
+//	return 0;
+//}
+/*
+class Student{
+public:
+	void SetStudentInfo(const char *name,const char *gender,int age){
+		strcpy(_name, name);
+		strcpy(_gender, gender);
+	}
+//private:
+	char _name[20];
+	int _age;
+	char _gender[2];
+}St;
+int main(){
+	St._age = 18;
+	cout << St._age << endl;
+	return 0;
+}
+*/
+
