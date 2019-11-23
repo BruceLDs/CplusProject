ef428c18df61936c237e2ffe481c75e2dbd04685
BruceLDs <2435324639@qq.com>
2019-11-23 21:41:03 +0800

    。。

---
+++ 字符串转换成整数/源.cpp
@@ -0,0 +1,45 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+	int StrToInt(string str)
+	{
+		if (str.empty())
+			return 0;
+
+		int symbol = 1;
+		if (str[0] == '-') //处理负号
+		{
+			symbol = -1;
+			str[0] = '0'; //这里是字符'0',不是0
+		}
+		else if (str[0] == '+') //处理正号
+		{
+			symbol = 1;
+			str[0] = '0';
+		}
+
+		int sum = 0;
+		for (int i = 0; i<str.size(); ++i)
+		{
+			if (str[i] < '0' || str[i] > '9')
+			{
+				sum = 0;
+				break;
+			}
+
+			sum = sum * 10 + str[i] - '0';
+
+		}
+		return symbol * sum;
+	}
+};
+
+int main(){
+	Solution s;
+	int ret = s.StrToInt("hfkfkdn");
+	cout << ret << endl;
+	return 0;
+}
