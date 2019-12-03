f321484497d6a688392c8d8cd250e007163bbb77
BruceLDs <2435324639@qq.com>
2019-12-03 23:22:25 +0800

    做了一些优化

--- 进程调度算法/源.cpp
+++ 进程调度算法/源.cpp
@@ -27,8 +27,12 @@ using namespace std;
 	float _weightedTuraroundTime;//带权周转时间
 	bool _state;//进程的状态
 };
+ void print(vector<pcb> p){
+	 cout << "进程名称	到达时间	服务时间	开始时间	结束时间	周转时间	带权周转时间		进程状态" << endl;
+	 for (auto& e : p)
+		 cout << e._name << "		" <<e._arriveTime << "			" << e._serviceTime << "		" << e._startTime << "	" << e._finishTime << "			" << e._turnaroundTime << "		" << e._weightedTuraroundTime << "		" << e._state << endl;
+ }
  void F_sort(vector<pcb> a){
-		 //判断是否有序
 		 int i = 0;
 		 int j = 0;
 		 int n = a.size();
@@ -49,7 +53,6 @@ using namespace std;
 		 }
 }
  void S_sort(vector<pcb> a){
-	 //判断是否有序
 	 int i = 0;
 	 int j = 0;
 	 int n = a.size();
@@ -82,24 +85,41 @@ using namespace std;
 		 cout << p[i]._name << "		" << p[i]._arriveTime << "			" << p[i]._serviceTime << "		" << p[i]._startTime << "	" << p[i]._finishTime << "			" << p[i]._turnaroundTime << "		" << p[i]._weightedTuraroundTime << "		" << p[i]._state << endl;
 		 k += p[i]._finishTime;
 	 }
+	 print(p);
  }
  void RR(vector<pcb> p, int k = 0){//时间片轮转
 	 int i = 0;
 	 int s = 0;
-	 while(1){
-		 p[i]._startTime = p[i]._arriveTime + k;
-		 p[i]._finishTime = p[i]._startTime + p[i]._serviceTime;
-		 p[i]._state = true;
-		 p[i]._turnaroundTime = p[i]._finishTime - p[i]._arriveTime;
-		 p[i]._weightedTuraroundTime = p[i]._turnaroundTime / p[i]._serviceTime;
-		 cout << p[i]._name << "正在CPU中" << endl;
-		 cout << "进程名称\t到达时间	\t服务时间\t开始时间\t结束时间\t周转时间\t带权周转时间\t进程状态" << endl;
-		 cout << p[i]._name << "\t" << p[i]._arriveTime << "\t" << p[i]._serviceTime << "\t" << p[i]._startTime << "	\t" << p[i]._finishTime << "\t" << p[i]._turnaroundTime << "\t" << p[i]._weightedTuraroundTime << "\t" << p[i]._state << endl;
-		 k += p[i]._finishTime;
-		 i++;
-		 if (i == p.size())
-			 i = 0;
+	 int n = 0;
+	 for (auto& e : p){
+	 s += e._serviceTime;
+	 n++;
 	 }
+	 vector<int> arr;
+	 arr.resize(n, 0);
+	 s += p[i]._arriveTime;
+	 p[0]._startTime = p[0]._arriveTime;
+	 while(i < s){
+		 if(i > 0)
+		 if (arr[i] != p[i]._serviceTime){
+			 p[i]._startTime = p[i - 1]._finishTime;
+			 p[i]._finishTime = p[i]._startTime + 1;
+			 p[i]._state = true;
+			 p[i]._turnaroundTime = p[i]._finishTime - p[i]._arriveTime;
+			 p[i]._weightedTuraroundTime = p[i]._turnaroundTime / p[i]._serviceTime;
+			 cout << p[i]._name << "正在CPU中" << endl;
+			 cout << "进程名称\t到达时间	\t服务时间\t开始时间\t结束时间\t周转时间\t带权周转时间\t进程状态" << endl;
+			 cout << p[i]._name << "\t" << p[i]._arriveTime << "\t" << p[i]._serviceTime << "\t" << p[i]._startTime << "	\t" << p[i]._finishTime << "\t" << p[i]._turnaroundTime << "\t" << p[i]._weightedTuraroundTime << "\t" << p[i]._state << endl;
+			 k += p[i]._finishTime;
+			 p[i]._state = false;
+			 arr[i]++;
+			 i++;
+		 }
+		 else{
+			 i++;
+		 }
+	 }
+	 print(p);
  }
  void SJF(vector<pcb> p, int k = 0){//短作业优先
 	 S_sort(p);
@@ -114,6 +134,7 @@ using namespace std;
 		 cout << p[i]._name << "		" << p[i]._arriveTime << "			" << p[i]._serviceTime << "		" << p[i]._startTime << "	" << p[i]._finishTime << "			" << p[i]._turnaroundTime << "		" << p[i]._weightedTuraroundTime << "		" << p[i]._state << endl;
 		 k += p[i]._finishTime;
 	 }
+	 print(p);
  }
  void PRT(vector<pcb> p, int k =0){//优先级调度
 	 for (int i = 0; i < p.size(); i++){
@@ -127,6 +148,7 @@ using namespace std;
 		 cout << p[i]._name << "		" << p[i]._arriveTime << "			" << p[i]._serviceTime << "		" << p[i]._startTime << "	" << p[i]._finishTime << "			" << p[i]._turnaroundTime << "		" << p[i]._weightedTuraroundTime << "		" << p[i]._state << endl;
 		 k += p[i]._finishTime;
 	 }
+	 print(p);
  }
  void menu(vector<pcb> p){
 	 cout << "***********************" << endl;
@@ -140,7 +162,8 @@ using namespace std;
 	 cout << "***********************" << endl;
 	 int n;
 	 float k = 0;
-	 cin >> n;
+	 while (1){
+		 cin >> n;
 		 switch (n){
 		 case 1:FCFS(p, k); break;
 		 case 2:RR(p, 0); break;
@@ -148,6 +171,7 @@ using namespace std;
 		 case 4:PRT(p, 0); break;
 		 default:break;
 		 }
+	 }
  }
  void test(){
 	 vector<pcb> p;
