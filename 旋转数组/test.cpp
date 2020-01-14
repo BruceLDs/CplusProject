d367aab13b59a0eca70eb08483aa137afb91507d
BruceLDs <2435324639@qq.com>
2020-01-14 22:56:39 +0800

    添加了一种O(n)算法

--- 旋转数组/test.cpp
+++ 旋转数组/test.cpp
@@ -14,7 +14,26 @@ void rmove(vector<int>& nums){
 	}
 	nums[0] = temp;
 }
-
+//O(n)Ëã·¨
+void rotate1(vector<int>& nums, int k) {
+	int i = (k - 1) % nums.size();
+	int j = (nums.size() - 1) % nums.size();
+	vector<int> newArr;
+	newArr.resize(nums.size());
+	while (i >= 0){
+		newArr[i] = nums[j];
+		j--;
+		i--;
+	}
+	i = k % nums.size();
+	j = 0;
+	while (i < nums.size()){
+		newArr[i] = nums[j];
+		i++;
+		j++;
+	}
+	nums = newArr;
+}
 void rotate(vector<int>& nums, int k) {
 	int i = 0;
 	while (i < k){
